e1b7f22d0e61708bc1ccd521d99896beed7f8883
TB15\Andrei <a@b.c>
2024-04-06 19:51:43 +0300

    mp3 data - add option to edit file property

--- mp3_data/main.py
+++ mp3_data/main.py
@@ -17,21 +17,42 @@ from pprint import pprint
 # print (audio)
 # os.rename(old, new)
 forbidden_chars_win = [ "<", ">", ":",  "/",   "\\",  "|", "?", "*",] #" ",
-mydir = r"C:\DOWNLOADS\yt_cb_out\manual_dw\_AUDIO\siemens_av_podcat"
+mydir = r'C:\DOWNLOADS\yt_cb_out\manual_dw\_AUDIO\_comedy\billMaher_podcast'
 myfiles = []
 for root, dirs, files in walk(mydir, topdown=False):
     myfiles =files
 
-for file in myfiles:
-    file = os.path.join(mydir, file)
-    pprint ( file)
-    audio = EasyID3(file)
-    new_name = audio['title'][0]
-    for f in forbidden_chars_win:
-        new_name = new_name.replace(f, " ")
-    new_name =os.path.join(mydir,new_name)
-    #exit()
-    new_name = new_name + ".mp3"
 
-    print (new_name)
-    os.rename(file, new_name)
+# ## Option 1 - 
+# ## extract the name from the "title" property and rename the file with it
+# for file in myfiles:
+#     file = os.path.join(mydir, file)
+#     pprint ( file)
+#     audio = EasyID3(file)
+#     new_name = audio['title'][0]
+#     for f in forbidden_chars_win:
+#         new_name = new_name.replace(f, " ")
+#     new_name =os.path.join(mydir,new_name)
+#     #exit()
+#     new_name = new_name + ".mp3"
+
+#     print (new_name)
+#     os.rename(file, new_name)
+
+
+# ## Option 2 - 
+# ## extract the name of the file and add the the "title" property
+for file in myfiles:
+    filepath = os.path.join(mydir, file)
+    pprint ( "setting title and author to " + file)
+    audio = EasyID3(filepath)
+    
+    #pprint (audio.keys())
+    
+    audio["title"] = file
+    audio["artist"] = "clubRandom"
+    audio.save()
+   
+  
+    
+  
